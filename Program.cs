2514e2931dbed95e16b4dc4fcefdcad37240da4d
Artem Artemev <artem@artemev.su>
2022-12-23 17:48:24 +0300

    1st lesson done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(rnd.Next(1, 10) + rnd.NextDouble(),2);
+        }
+
+    }
+}
+double[,] matrix = new double[5, 5];
+
+PrintArray(matrix);
+Console.WriteLine("--------------------------");
+FillArray(matrix);
+PrintArray(matrix);
